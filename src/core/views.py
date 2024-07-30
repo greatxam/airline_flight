f0e7f1c142a388c6366ced03e55089b761e36145
Maximillian Estrada <maximillian_estrada@yahoo.com>
2024-07-30 18:00:35 +0800

    v0.0.5 - Flight Page
    - Flight list
    
    Closes #5

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,16 @@
 # Airline Flight
 # Created by Maximillian M. Estrada on 2024-07-30
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from core.models import Flight
 
 
 class DashboardView(TemplateView):
     template_name = 'core/index.html'
+
+
+class FlightView(ListView):
+    model = Flight
+    template_name = 'core/flight.html'
+
